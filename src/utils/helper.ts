e1fd552b705f91bf217befee445f89fbb781200e
coooo77 <coooo77@hotmail.com>
2025-05-24 21:34:04 +0800

    fix: better regex

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,6 @@
 'use strict'
 export function extractDateFromFilename(filename: string) {
-  const dateTimeRegex = /_(\d{8})_(\d{6})/
+  const dateTimeRegex = /(\d{4}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01]))_((?:[01]\d|2[0-3])[0-5]\d[0-5]\d)/
   const match = filename.match(dateTimeRegex)
 
   if (!match) return null
