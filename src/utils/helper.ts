dc358327801fc7bdcea49db6f911296b66e50be8
coooo77 <coooo77@hotmail.com>
2025-06-04 22:31:10 +0800

    feat: better regex to get date

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,6 @@
 'use strict'
 export function extractDateFromFilename(filename: string) {
-  const dateTimeRegex = /(\d{4}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01]))_((?:[01]\d|2[0-3])[0-5]\d[0-5]\d)/
+  const dateTimeRegex = /_(20\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01]))_((?:[01]\d|2[0-3])[0-5]\d[0-5]\d)/
   const match = filename.match(dateTimeRegex)
 
   if (!match) return null
